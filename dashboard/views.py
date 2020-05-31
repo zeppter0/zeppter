6f517f4887bcd2649c060969b9e8abd1501fb018
devan mandal <zeppter0@gmail.com>
2020-05-31 18:34:31 +0530

    my all file change

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def dashboard(request):
+    return render(request,'dashboard/main.html')
+    #return HttpResponse("hello")
