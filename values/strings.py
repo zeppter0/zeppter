602ad5696a8af32c48f176c6a29d4b9f0b06d6c8
Devan Mandal <zeppter0gmail.com>
2020-10-13 11:35:26 +0530

    plopleadmin stuents dashboard

--- values/strings.py
+++ values/strings.py
@@ -3,7 +3,10 @@ def bootstrap():
         "bootstrapcss" : "assets/bootstrap/css/bootstrap.min.css",
         "jquery": "assets/jquery/mainjquery.js",
         "popper": "assets/pooper/popper.min.js",
-        "bootstrapjs" : "assets/bootstrap/js/bootstrap.min.js"
+        "bootstrapjs" : "assets/bootstrap/js/bootstrap.min.js",
+        "plopleadmin" : "assets/plopleadmin/plopleadmin.css",
+        "googleicon": "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "fontawesome": "assets/fontawesome/css/all.css",
     }
 
 
