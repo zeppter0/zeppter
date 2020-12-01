061c834b84950e01544ec516520974d4f4f22009
devan mandal <zeppter0@gmail.com>
2020-12-01 14:08:12 +0530

    change database

--- zeppter/settings.py
+++ zeppter/settings.py
@@ -110,14 +110,13 @@ WSGI_APPLICATION = 'zeppter.wsgi.application'
 
 DATABASES = {
 
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
-
-
-
-
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'zeppter',
+        'USER': 'dkboss650',
+        'PASSWORD': 'Sorry9023@',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 
 }
