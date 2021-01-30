6438331c7fbb099407911f95bbaf72e6f4c53136
devan mandal <zeppter0@gmail.com>
2021-01-30 07:22:26 +0530

    fd

--- zeppter/urls.py
+++ zeppter/urls.py
@@ -29,6 +29,8 @@ url('yandex_47dbe3c258eabd6f.html', TemplateView.as_view(template_name='include/
                                                 content_type='text/plain')),
     url('BingSiteAuth.xml',TemplateView.as_view(template_name='include/BingSiteAuth.xml',
                                                 content_type='text/plain')),
+    path("googleaec1d028d595b8d4.html" ,TemplateView.as_view(template_name='include/googleaec1d028d595b8d4.html',
+                                                content_type='text/plain')),
 # path("admin/<str:id>",include("plopleadmin.urls")),
     path("user/",include("myuser.urls")),
 # path("robots.txt",views.robots ,name="roboot.txt"),
