de3f5425a8209e63f6c8fd13ec9e8ae53d217561
devan mandal <zeppter0@gmail.com>
2021-02-02 07:04:33 +0530

    script problem

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -47,7 +47,7 @@ path('content/<str:url>/', views.shodata, name="content"),
    # url('sitemap.xml',views.sitema, name='django.contrib.sitemaps.views.sitemap')
 
 
-    path("sitemap.xml/", sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
+    path("sitemap.xml", sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 
 
 
