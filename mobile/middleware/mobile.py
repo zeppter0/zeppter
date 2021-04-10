9bd962baf580cea0813c23860afcdf252a2978ac
devan mandal <zeppter0@gmail.com>
2021-04-10 17:40:14 +0530

    mobile size problem fix

--- mobile/middleware/mobile.py
+++ mobile/middleware/mobile.py
@@ -30,13 +30,13 @@ class IS_MOBILE(object):
         ua = request.META.get('HTTP_USER_AGENT', '').lower()
 
         if ua.find("iphone") > 0:
-            device['iphone'] = "iphone" + re.search("iphone os (\d)", ua).groups(0)[0]
+            device['iphone'] = "iphone"
 
         if ua.find("ipad") > 0:
             device['ipad'] = "ipad"
 
         if ua.find("android") > 0:
-            device['android'] = "android" + re.search("android (\d\.\d)", ua).groups(0)[0]
+            device['android'] = "android"
 
         # spits out device names for CSS targeting, to be applied to <html> or <body>.
         device['classes'] = " ".join(v for (k, v) in device.items())
