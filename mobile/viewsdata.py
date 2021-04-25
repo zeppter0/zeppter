17746c4cac56ee5aa8161e7fc285ca9855c9d368
devan mandal <zeppter0@gmail.com>
2021-04-25 23:59:32 +0530

    fix pdf file show

--- mobile/viewsdata.py
+++ mobile/viewsdata.py
@@ -1,9 +1,19 @@
 import mimetypes
+from typing import Text
 from wsgiref.util import FileWrapper
 
 from appdirs import unicode
 from django.views.generic import View
 from django.http import FileResponse, HttpResponse, Http404
+from reportlab.lib.colors import Color
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import cm, mm
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
+from reportlab.platypus import BaseDocTemplate, PageTemplate, Image, Paragraph, Frame
+
 from zeppter import settings
 from fpdf import FPDF
 import os.path
@@ -11,8 +21,13 @@ import pathlib
 from fpdf import FPDF,HTMLMixin
 from admin_dashboard.models import Book
 from django.utils.encoding import iri_to_uri, smart_str
+from bs4 import BeautifulSoup
 
 from django.utils.html import strip_tags
+
+mobile_size = (120 * mm, 297 * mm)
+
+
 class DownloadPDF(View):
     def post(self,request):
         book_id = request.POST.get("bookid")
@@ -29,110 +44,79 @@ class DownloadPDF(View):
 
 
     def pdfreader(self,book):
-       # title = request.POST.get("title")[:20]
-
-       # print(strip_tags(book.book_data))
 
 
+        style = getSampleStyleSheet()['Normal']
+        style.fontName = 'Montserratd'
+        #style.spaceBefore = 20
+        style.leading=24
+        style.spaceBefore = 30
 
 
+        elements = []
 
-        pdf_path = "testkese.pdf"
-        server_webpath = "/var/www/zeppter/"
-
-        pdf = PDF('P','mm',(200,500))
-        pdf.alias_nb_pages()
-        pdf.add_page()
-        pdf.add_font("HidiShow", "" ,str(settings.BASE_DIR)+"/static/assets/font/gargi.ttf",uni=True)
-        pdf.set_font("HidiShow", "",25)
-        
-        image_url = settings.MEDIA_ROOT+'/'+str(book.book_image)
-        pdf.write(15,book.book_title)
-        pdf.ln(30)
+        doc = BaseDocTemplate(settings.BASE_DIR+'/media/downloadpdf/'+str(book.pk)+'.pdf', showBoundary=1, pagesize=mobile_size)
+        pdfmetrics.registerFont(TTFont('Montserrat', 'fonts/Hind-SemiBold.ttf'))
+        pdfmetrics.registerFont(TTFont('Montserratd', 'fonts/Martel-ExtraLight.ttf'))
 
 
-        # pdf.cell(0,0,book.book_title)
+        frameT = Frame(5 * mm,5*mm,110*mm,287 *mm,showBoundary=2)
+       # TopCenter = Frame(1.2 * inch, height - 1.2 * inch, width - 2.4 * inch, 1 * inch, showBoundary=1, id='normal')
 
-       #  pdf.print_chapter(image_url,book.book_title,strip_tags(book.book_data))
-     #   for titi in book.book_title.split('\n'):
-     #       pdf.write(7,titi)
-     #       pdf.ln(20)
+        frame1 = Frame(0,0, doc.width / 2 - 3, doc.height, id='col1')
+        frame2 = Frame(doc.leftMargin + doc.width , doc.bottomMargin, doc.width , doc.height, id='col2')
 
-      #  pdf.write(9,book.book_title)
+        doc.addPageTemplates([
+            PageTemplate(id='basedoc', frames=frameT),
+       #     PageTemplate(id='TwoCol', frames=[frame1, frame2]),
+        ])
+        im = Image(settings.BASE_DIR+'/media/'+str(book.book_image),80* mm,200)
+        styles = getSampleStyleSheet()
+        yourStyle = ParagraphStyle('yourtitle',
+                                   fontName="Montserrat",
+                                   fontSize=16,
+                                   #       parent=style['Heading2'],
+                                   alignment=1,
+                                   spaceAfter=30)
+        # styletitle.alignment = TA_CENTER
 
-       # pdf.set_left_margin(20)
-        if os.path.isfile(image_url):
-             pdf.image(image_url,pdf.get_x(),pdf.get_y(),100)
-        print(book.book_image)
-        pdf.ln(70)
-        textg = strip_tags(book.book_data)
+        elements.append(Paragraph(book.book_title, yourStyle))
+        elements.append(im)
+        elements.append(Paragraph(book.book_data, style))
 
-        pdf.add_font("HidiSh", "",str(settings.BASE_DIR)+"/static/assets/font/gargi.ttf", uni=True)
-        pdf.set_font("HidiSh" ,"" ,18)
-
-        for tedt in textg.split('\n'):
-           pdf.write(12,tedt)
-           
-
-
-
-
-        pdf.output(str(settings.BASE_DIR)+'/media/downloadpdf/'+str(book.pk)+'.pdf', 'F')
-
-        if pathlib.Path(pdf_path).exists() :
-            print("no file data")
-
-        return HttpResponse("upload complite")
-
+        # start the construction of the pdf
+        doc.build(elements, canvasmaker=NumberedCanvas)
+        return HttpResponse("writer is susesss")
 
 # return FileResponse(pdf_path)
 
 
 
-class PDF(FPDF,HTMLMixin):
-    def header(self):
-        # Logo
-    #    self.image('logo_pb.png', 10, 8, 33)
-        # Arial bold 15
-        self.set_font('Arial', 'B', 15)
-        # Move to the right
-        self.cell(80)
-        # Title
-      #  self.cell(30, 10, 'Title', 1, 0, 'C')
-        # Line break
-        self.ln(20)
+class NumberedCanvas(canvas.Canvas):
+            def __init__(self, *args, **kwargs):
+                canvas.Canvas.__init__(self, *args, **kwargs)
+                self._saved_page_states = []
 
-    # Page footer
-    def footer(self):
-        # Position at 1.5 cm from bottom
-        self.set_y(-15)
-        # Arial italic 8
-        self.set_font('Arial', 'I', 8)
-        # Page number
-        self.cell(0, 10, 'Page ' + str(self.page_no()) + '/{nb}', 0, 0, 'C')
+            def showPage(self):
+                self._saved_page_states.append(dict(self.__dict__))
+                self._startPage()
 
+            def save(self):
+                """add page info to each page (page x of y)"""
+                num_pages = len(self._saved_page_states)
+                for state in self._saved_page_states:
+                    self.__dict__.update(state)
+                    self.draw_page_number(num_pages)
+                    canvas.Canvas.showPage(self)
+                canvas.Canvas.save(self)
 
-    def my_title(self,title):
-         self.set_text_color("#000000")
-         self.set_font_size("30")
-
-         self.cell(txt="hello",align="C")
-
-    def getmyimage(self,myimage):
-        self.c_margin()
-        self.cell(0,0,)
-       # self.image(settings.BASE_DIR+"/media/"+str(myimage),0, 0, 0)
-    def maincontent(self,datashow):
-        for txt in datashow.split('\n'):
-            self.write(8,txt)
-            self.ln(8)
-
-    def print_chapter(self, image, title, data):
-        self.add_page()
-        self.my_title(title)
-
-        self.getmyimage(image)
-        self.maincontent(data)
+            def draw_page_number(self, page_count):
+                self.setFont('Montserrat', 9)
+                self.setLineWidth(0.1)
+                self.setStrokeColor(Color(0, 0, 0, alpha=0.2))
+                self.setFillColor(Color(0, 0, 0, alpha=0.4))
+                #self.line(cm, 1.5 * cm, A4[0] - cm, 1.5 * cm)
+                self.drawRightString(mobile_size[0] -cm,3, "Page %d of %d" % (self._pageNumber, page_count))
 
 
 
