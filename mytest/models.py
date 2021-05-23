2146a06402a4fdee6eaf685346e7d61e1d028168
devan mandal <zeppter0@gmail.com>
2021-05-23 06:34:19 +0530

    hi

--- mytest/models.py
+++ mytest/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Reddit(models.Model):
+    name = models.CharField(max_length=10)
+    position = models.IntegerField()
--- mytest/reddit.py
+++ mytest/reddit.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect('db/reddit.db',check_same_thread=False)
+conn = sqlite3.connect('db/reddit.db',check_same_thread=True)
 
 
 class Reddit:
